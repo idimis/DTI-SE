aaf82a55bd40901ace081acc04daf330cedd4950
idimis <immanuel.adimas@gmail.com>
2024-09-05 12:27:05 +0700

    only showing exercise 3 solution

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,9 @@
-import com.adimas.exercise.day2.Exercise2;
 import com.adimas.exercise.day3.Exercise3;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Exercise2.run();
         Exercise3.run();
 
 
