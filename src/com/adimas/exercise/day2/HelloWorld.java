f4cda5818eb84d40e3bdaf8910e3a3b11b3287db
idimis <immanuel.adimas@gmail.com>
2024-09-05 15:33:37 +0700

    showing only solution

--- src/com/adimas/exercise/day2/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package com.adimas.exercise.day2;
-
-public class HelloWorld {
-    public static void run() {
-        System.out.println("Hello, World!");
-        System.out.println("Welcome to the new feature branch!");
-    }
-}
