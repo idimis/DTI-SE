c3afd09fb5b3d8edb68f3c3140e81ec14ab41c82
idimis <immanuel.adimas@gmail.com>
2024-09-05 15:26:47 +0700

    add exercise 4

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,18 @@
 import com.adimas.exercise.day2.Exercise2;
 import com.adimas.exercise.day3.Exercise3;
+import com.adimas.exercise.day4.Exercise4;
+
+
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
     public static void main(String[] args) {
-        Exercise2.run();
-        Exercise3.run();
+       // Exercise2.run();
+       // Exercise3.run();
+            Exercise4.run();
+
 
 
 
