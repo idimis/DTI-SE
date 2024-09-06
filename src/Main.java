726425ebc7ed5f7e4c8d984bf392011a9c9fc0ca
idimis <immanuel.adimas@gmail.com>
2024-09-06 17:25:50 +0700

    delete other days

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,3 @@
-import com.adimas.exercise.day2.Exercise2;
-import com.adimas.exercise.day3.Exercise3;
-import com.adimas.exercise.day4.Exercise4;
 import com.adimas.exercise.day5.Exercise5;
 
 
@@ -10,9 +7,6 @@ import com.adimas.exercise.day5.Exercise5;
 public class Main {
 
     public static void main(String[] args) {
-            Exercise2.run();
-            Exercise3.run();
-            Exercise4.run();
             Exercise5.run();
             System.out.println("test.");
 
