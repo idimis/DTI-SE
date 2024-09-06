048afa3ff73d45909ac43c670ac4cbb4d17267f1
idimis <immanuel.adimas@gmail.com>
2024-09-06 17:06:31 +0700

    add new exercise 5

---
+++ src/com/adimas/exercise/day5/Exercise5.java
@@ -0,0 +1,14 @@
+package com.adimas.exercise.day5;
+
+import java.util.Scanner;
+import java.util.Random;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Exercise5 {
+
+    public static void run() {
+        MultiTaskProgram program = new MultiTaskProgram();
+        program.run();
+    }
+}
