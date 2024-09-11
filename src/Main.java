00c0e6e5ade800126579f18cf806764447430869
idimis <immanuel.adimas@gmail.com>
2024-09-11 17:25:30 +0700

    tidy up branch exercise8

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,8 @@
 
-import com.adimas.exercise.day8.Exercise8;
-
 
 public class Main {
 
     public static void main(String[] args) {
-
             Exercise8.run();
             System.out.println("test.");
 
