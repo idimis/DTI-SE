d821b89d50d3d39f7e1b042a091d3a8bbada95fd
idimis <immanuel.adimas@gmail.com>
2024-09-11 22:36:26 +0700

    tidy up exercise8

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,11 @@
-import com.adimas.exercise.day2.Exercise2;
-import com.adimas.exercise.day3.Exercise3;
-import com.adimas.exercise.day4.Exercise4;
-import com.adimas.exercise.day5.Exercise5;
-import com.adimas.exercise.day6.Exercise6;
-import com.adimas.exercise.day7.Exercise7;
+
 import com.adimas.exercise.day8.Exercise8;
 
 
 public class Main {
 
     public static void main(String[] args) {
-            Exercise2.run();
-            Exercise3.run();
-            Exercise4.run();
-            Exercise5.run();
-            Exercise6.run();
-            Exercise7.run();
+
             Exercise8.run();
             System.out.println("test.");
 
