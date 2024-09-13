27a580476eea80d0ac8e7a8db2fb1603e9799662
idimis <immanuel.adimas@gmail.com>
2024-09-13 16:32:31 +0700

    only show exer9

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,3 @@
-import com.adimas.exercise.day2.Exercise2;
-import com.adimas.exercise.day3.Exercise3;
-import com.adimas.exercise.day4.Exercise4;
-import com.adimas.exercise.day5.Exercise5;
-import com.adimas.exercise.day6.Exercise6;
-import com.adimas.exercise.day7.Exercise7;
-import com.adimas.exercise.day8.Exercise8;
 import com.adimas.exercise.day9.Exercise9;
 
 
@@ -13,7 +6,6 @@ public class Main {
     public static void main(String[] args) {
             Exercise9.run();
             System.out.println("test.");
-
 
 
 
--- src/com/adimas/exercise/day2/DistanceConverter.java
+++
@@ -1,9 +0,0 @@
-package com.adimas.exercise.day2;
-
-public class DistanceConverter {
-    public static void run() {
-        int centimeters = 100000;
-        double kilometers = centimeters / 100000.0;
-        System.out.println(centimeters + " cm = " + kilometers + " km");
-    }
-}
