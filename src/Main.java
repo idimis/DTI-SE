ecda0f12e7f6953aa8c922a49f97c223f547fbea
idimis <immanuel.adimas@gmail.com>
2024-09-13 19:42:29 +0700

    only show exercise10

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,10 @@
-import com.adimas.exercise.day2.Exercise2;
-import com.adimas.exercise.day3.Exercise3;
-import com.adimas.exercise.day4.Exercise4;
-import com.adimas.exercise.day5.Exercise5;
-import com.adimas.exercise.day6.Exercise6;
-import com.adimas.exercise.day7.Exercise7;
-import com.adimas.exercise.day8.Exercise8;
-import com.adimas.exercise.day9.Exercise9;
 import com.adimas.exercise.day10.Exercise10;
 
 
 public class Main {
 
     public static void main(String[] args) {
-           // Exercise2.run();
-           // Exercise3.run();
-           // Exercise4.run();
-           // Exercise5.run();
-           // Exercise6.run();
-           // Exercise7.run();
-           // Exercise8.run();
-           // Exercise9.run();
+
             Exercise10.run();
             System.out.println("test.");
 
